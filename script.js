f85a5aca8891b595015be476ff98737082c8e61c
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-10 18:22:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const number = document.getElementById('number');
+const email = document.getElementById('email');
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+	alert(`First Name: ${first.value}
+Last Name: ${last.value}
+Phone Number: ${number.value}
+Email ID: ${email.value}`);
+})
